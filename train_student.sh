402eac93a17ac99a7ea486de43978d6522d0f580
Hanhai <1932232849@qq.com>
2023-04-30 01:06:01 +0800

    train_student.sh的输出到train.log中

--- train_student.sh
+++ train_student.sh
@@ -1,2 +1,2 @@
-python -u s_t_shapes.py --gpu_id 0 --preprocess scale_width_and_crop
-python -u train_student.py --gpu_id 0 --preprocess scale_width_and_crop
+(python -u s_t_shapes.py --gpu_id 0 --preprocess scale_width_and_crop && \
+python -u train_student.py --gpu_id 0 --preprocess scale_width_and_crop) >train.log 2>&1
